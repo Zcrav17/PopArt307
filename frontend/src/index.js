0136d5b0e323c7946cd5299ba69f3e7f9300c015
wacky@Zacs-Desktop <zcraven17@gmail.com>
2020-11-03 17:38:53 -0700

    updated webpage

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import App from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { unregister } from 'registerServiceWorker';
+import SettingsService from 'modules/settings/settingsService';
+// @ts-ignore
+import { i18n } from 'i18n';
+
+(async function() {
+  document.title = i18n('app.title');
+  await SettingsService.fetchAndApply();
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+  /**
+   * It may cause cache issues when developing, so, after your app is ready,
+   * change those lines
+   */
+  unregister();
+  // registerServiceWorker();
+})();
